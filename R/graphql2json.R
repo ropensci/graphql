38f125963bdd9719ae47b01f927a18c29d71016d
Jeroen Ooms <jeroenooms@gmail.com>
2016-10-04 10:57:56 +0200

    Fixes for CMD check

--- R/graphql2json.R
+++ R/graphql2json.R
@@ -1,10 +1,16 @@
 #' Dump GraphQL to JSON
 #'
-#' Parses GraphQL and exports as JSON
+#' Parses GraphQL and exports the AST in JSON format.
 #'
 #' @export
 #' @useDynLib graphql
 #' @importFrom Rcpp sourceCpp
-graphql2json <- function(string){
-  dump_json_ast(string)
+#' @importFrom jsonlite toJSON
+#' @param input a string with graphql syntax
+#' @examples graphql2json("{ field(complex: { a: { b: [ $var ] } }) }")
+graphql2json <- function(input){
+  stopifnot(is.character(input))
+  input <- paste(input, collapse = "\n")
+  json <- dump_json_ast(input)
+  structure(json, class = "json")
 }
