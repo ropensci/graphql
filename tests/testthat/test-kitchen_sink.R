b0fa6c257861bc985fd124691765dc9996271ae3
Barret Schloerke <schloerke@gmail.com>
2017-01-11 10:26:06 -0500

    Update tests to support schema definitions

--- tests/testthat/test-kitchen_sink.R
+++ tests/testthat/test-kitchen_sink.R
@@ -11,3 +11,16 @@ test_that("kitchen sink parses", {
   expect_true(is.list(result))
 
 })
+
+
+test_that("kitchen sink parses with schema functionality", {
+
+  kitchen_txt <- paste(readLines("kitchen-sink.graphql"), collapse = "\n")
+
+  json_txt <- schema2json(kitchen_txt)
+
+  result <- jsonlite::fromJSON(json_txt)
+
+  expect_true(is.list(result))
+
+})
--- tests/testthat/test-schema_kitchen_sink.R
+++ tests/testthat/test-schema_kitchen_sink.R
@@ -1,10 +1,18 @@
 context("kitchen_sink_schema")
 
+test_that("kitchen sink schema fails to parse with query only support", {
+
+  kitchen_schema_txt <- paste(readLines("schema-kitchen-sink.graphql"), collapse = "\n")
+
+  expect_error({graphql2json(kitchen_schema_txt)}, "schema support disabled")
+})
+
+
 test_that("kitchen sink schema parses", {
 
   kitchen_schema_txt <- paste(readLines("schema-kitchen-sink.graphql"), collapse = "\n")
 
-  json_txt <- graphql2json(kitchen_schema_txt)
+  json_txt <- schema2json(kitchen_schema_txt)
 
   result <- jsonlite::fromJSON(json_txt)
 
