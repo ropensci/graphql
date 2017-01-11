acc8fc0416705c6ab0c6caa42edf8e86368ffa39
Barret Schloerke <schloerke@gmail.com>
2017-01-11 10:25:46 -0500

    Add function that supports schema definitions

--- R/graphql2json.R
+++ R/graphql2json.R
@@ -1,6 +1,6 @@
-#' Dump GraphQL to JSON
+#' Dump GraphQL Queries to JSON
 #'
-#' Parses GraphQL and exports the AST in JSON format.
+#' Parses GraphQL queries and exports the AST in JSON format.
 #'
 #' @export
 #' @useDynLib graphql
@@ -14,5 +14,24 @@ graphql2json <- function(input){
   stopifnot(is.character(input))
   input <- paste(input, collapse = "\n")
   json <- dump_json_ast(input)
+  structure(json, class = "json")
+}
+
+#' Dump All GraphQL to JSON
+#'
+#' Parses GraphQL queries and schema definitions and exports the AST in JSON format.
+#'
+#' @export
+#' @useDynLib graphql
+#' @rdname graphql_schema
+#' @aliases graphql_schema
+#' @importFrom Rcpp sourceCpp
+#' @importFrom jsonlite toJSON
+#' @param input a string with graphql syntax
+#' @examples graphql2json("schema { query: QueryType }")
+schema2json <- function(input){
+  stopifnot(is.character(input))
+  input <- paste(input, collapse = "\n")
+  json <- dump_schema_json_ast(input)
   structure(json, class = "json")
 }
